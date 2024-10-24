f4235adc73479fc0702840943add1022f230cc7c
Grigory Taran <grunder-96@mail.ru>
2024-10-24 13:21:01 +0300

    feature: add SQL DML script

---
+++ src/main/resources/sql/insert.sql
@@ -0,0 +1,34 @@
+INSERT INTO players(name) VALUES ('Roger Federer')
+INSERT INTO players(name) VALUES ('Rafael Nadal')
+INSERT INTO players(name) VALUES ('Novak Djokovic')
+INSERT INTO players(name) VALUES ('Maria Sharapova')
+INSERT INTO players(name) VALUES ('Serena Williams')
+INSERT INTO players(name) VALUES ('Andre Agassi')
+INSERT INTO players(name) VALUES ('Pete Sampras')
+INSERT INTO players(name) VALUES ('Kim Clijsters')
+INSERT INTO players(name) VALUES ('Stan Wawrinka')
+INSERT INTO players(name) VALUES ('Martina Navratilova')
+INSERT INTO players(name) VALUES ('Bjorn Borg')
+INSERT INTO players(name) VALUES ('Andy Murray')
+
+INSERT INTO matches(player1, player2, winner) VALUES (1, 2, 1) -- Federer vs Nadal, Winner: Federer
+INSERT INTO matches(player1, player2, winner) VALUES (3, 4, 3) -- Djokovic vs Sharapova, Winner: Djokovic
+INSERT INTO matches(player1, player2, winner) VALUES (5, 6, 5) -- Williams vs Agassi, Winner: Williams
+INSERT INTO matches(player1, player2, winner) VALUES (7, 8, 7) -- Sampras vs Clijsters, Winner: Sampras
+INSERT INTO matches(player1, player2, winner) VALUES (9, 10, 10) -- Wawrinka vs Navratilova, Winner: Navratilova
+INSERT INTO matches(player1, player2, winner) VALUES (11, 12, 12) -- Borg vs Murray, Winner: Murray
+INSERT INTO matches(player1, player2, winner) VALUES (1, 3, 3) -- Federer vs Djokovic, Winner: Djokovic
+INSERT INTO matches(player1, player2, winner) VALUES (2, 4, 2) -- Nadal vs Sharapova, Winner: Nadal
+INSERT INTO matches(player1, player2, winner) VALUES (5, 7, 5) -- Williams vs Sampras, Winner: Williams
+INSERT INTO matches(player1, player2, winner) VALUES (8, 9, 8) -- Clijsters vs Wawrinka, Winner: Clijsters
+INSERT INTO matches(player1, player2, winner) VALUES (10, 11, 10) -- Navratilova vs Borg, Winner: Navratilova
+INSERT INTO matches(player1, player2, winner) VALUES (1, 4, 1) -- Federer vs Sharapova, Winner: Federer
+INSERT INTO matches(player1, player2, winner) VALUES (2, 3, 2) -- Nadal vs Djokovic, Winner: Nadal
+INSERT INTO matches(player1, player2, winner) VALUES (6, 8, 6) -- Agassi vs Clijsters, Winner: Agassi
+INSERT INTO matches(player1, player2, winner) VALUES (9, 11, 9) -- Wawrinka vs Borg, Winner: Wawrinka
+INSERT INTO matches(player1, player2, winner) VALUES (10, 12, 12) -- Navratilova vs Murray, Winner: Murray
+INSERT INTO matches(player1, player2, winner) VALUES (1, 5, 1) -- Federer vs Williams, Winner: Federer
+INSERT INTO matches(player1, player2, winner) VALUES (7, 10, 10) -- Sampras vs Navratilova, Winner: Navratilova
+INSERT INTO matches(player1, player2, winner) VALUES (3, 6, 3) -- Djokovic vs Agassi, Winner: Djokovic
+INSERT INTO matches(player1, player2, winner) VALUES (2, 12, 2) -- Nadal vs Murray, Winner: Nadal
+INSERT INTO matches(player1, player2, winner) VALUES (4, 9, 4) -- Sharapova vs Wawrinka, Winner: Sharapova
