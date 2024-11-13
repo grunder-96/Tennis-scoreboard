f94050c0c8d6c53307f496684bfbb5954c24941e
Grigory Taran <grunder-96@mail.ru>
2024-11-13 10:02:17 +0300

    feature: add MainController

---
+++ src/main/java/org/edu/pet/controller/MainController.java
@@ -0,0 +1,19 @@
+package org.edu.pet.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.edu.pet.util.JspHelper;
+
+import java.io.IOException;
+
+@WebServlet("")
+public class MainController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher(JspHelper.getPath("index")).forward(req, resp);
+    }
+}
