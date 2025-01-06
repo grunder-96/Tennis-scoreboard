9899f23fcf35037fbe5bc160d7334edd7e5c25ea
Grigory Taran <grunder-96@mail.ru>
2025-01-06 10:22:35 +0300

    feature: implements createMatch-method logic

--- src/main/java/org/edu/pet/service/OngoingMatchesService.java
+++ src/main/java/org/edu/pet/service/OngoingMatchesService.java
@@ -1,15 +1,18 @@
 package org.edu.pet.service;
 
 import org.edu.pet.dto.CreateMatchDto;
-import org.edu.pet.entity.Match;
+import org.edu.pet.dto.match.MatchScore;
+import org.edu.pet.dto.match.PlayerScore;
 import org.edu.pet.entity.Player;
+import org.edu.pet.exception.OngoingMatchNotFoundException;
 
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class OngoingMatchesService {
 
-    private static final ConcurrentHashMap<UUID, Match> ONGOING_MATCHES = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<UUID, MatchScore> ONGOING_MATCHES = new ConcurrentHashMap<>();
     
     private final PlayerService playerService = new PlayerService();
 
@@ -19,6 +22,28 @@ public class OngoingMatchesService {
 
         UUID uuid = UUID.randomUUID();
 
-        MatchScore
+        MatchScore matchScore = MatchScore.builder()
+                .firstPlayerScore(
+                        PlayerScore.builder()
+                                .player(firstPlayer)
+                                .build()
+                )
+                .secondPlayerScore(
+                        PlayerScore.builder()
+                                .player(secondPlayer)
+                                .build()
+                )
+                .build();
+
+        ONGOING_MATCHES.put(uuid, matchScore);
+
+        return uuid;
+    }
+
+    public MatchScore getMatchScore(UUID uuid) {
+        return Optional.ofNullable(ONGOING_MATCHES.get(uuid))
+                .orElseThrow(() -> new OngoingMatchNotFoundException(
+                        "Ongoing match with uuid %s not found".formatted(uuid)
+                ));
     }
 }
