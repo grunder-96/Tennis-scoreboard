083a2c1025455ab7ece4ad4e6ac6d91baa97900d
Grigory Taran <grunder-96@mail.ru>
2025-01-22 13:28:28 +0300

    refactor: add annotations for correct mapping

--- src/main/java/org/edu/pet/mapper/PlayerMapper.java
+++ src/main/java/org/edu/pet/mapper/PlayerMapper.java
@@ -1,14 +1,21 @@
 package org.edu.pet.mapper;
 
 import org.edu.pet.dto.PlayerDto;
+import org.edu.pet.dto.match.MatchPlayer;
+import org.edu.pet.dto.match.PlayerScore;
 import org.edu.pet.entity.Player;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(imports = PlayerScore.class)
 public interface PlayerMapper {
 
     PlayerMapper INSTANCE = Mappers.getMapper(PlayerMapper.class);
-    
+
+    @Mapping(target = "id", ignore = true)
     Player fromPlayerDto(PlayerDto playerDto);
+
+    @Mapping(target = "playerScore", expression = "java(new PlayerScore())")
+    MatchPlayer toMatchPlayer(Player player);
 }
