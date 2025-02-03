26bd709cc671b2d1c9fe1e1cc6c37a95747f59f8
Grigory Taran <grunder-96@mail.ru>
2025-02-03 14:18:46 +0300

    feature: add SharedRepository implementation

---
+++ src/main/java/org/edu/pet/repository/SharedRepositoryImpl.java
@@ -0,0 +1,29 @@
+package org.edu.pet.repository;
+
+import org.edu.pet.util.HibernateUtil;
+import org.edu.pet.util.TransactionUtil;
+import org.hibernate.Session;
+
+import java.lang.reflect.Proxy;
+
+public class SharedRepositoryImpl<E> implements SharedRepository<E> {
+
+    protected final Session session = getCurrentSession();
+
+    @Override
+    public E save(E entity) {
+        return TransactionUtil.executeInTransaction(
+                session -> {
+                    session.persist(entity);
+                    return entity;
+                },
+                session
+        );
+    }
+
+    private Session getCurrentSession() {
+        return (Session) Proxy.newProxyInstance(HibernateUtil.class.getClassLoader(), new Class[]{Session.class},
+                (proxy, method, args) -> method.invoke(HibernateUtil.getSessionFactory().getCurrentSession(), args));
+
+    }
+}
