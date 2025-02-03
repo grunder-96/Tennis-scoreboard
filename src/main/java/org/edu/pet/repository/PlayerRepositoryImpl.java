e99a00ddbe2d4b3b33d1e857cce361dee668f1e1
Grigory Taran <grunder-96@mail.ru>
2025-02-03 14:20:55 +0300

    refactor: now PlayerRepositoryImpl extends SharedRepositoryImpl and uses TransactionUtil

--- src/main/java/org/edu/pet/repository/PlayerRepositoryImpl.java
+++ src/main/java/org/edu/pet/repository/PlayerRepositoryImpl.java
@@ -1,43 +1,19 @@
 package org.edu.pet.repository;
 
 import org.edu.pet.model.entity.Player;
-import org.edu.pet.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import org.edu.pet.util.TransactionUtil;
 
 import java.util.Optional;
 
-public class PlayerRepositoryImpl implements PlayerRepository {
-
-    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+public class PlayerRepositoryImpl extends SharedRepositoryImpl<Player> implements PlayerRepository {
 
     @Override
     public Optional<Player> findByPlayerName(String playerName) {
-        Session session = sessionFactory.getCurrentSession();
-
-        Transaction transaction = session.beginTransaction();
-
-        Optional<Player> maybePlayer = session
-                .createQuery("select p from Player p  where p.name = :name", Player.class)
-                .setParameter("name", playerName)
-                .uniqueResultOptional();
-
-        session.getTransaction().commit();
-
-        return maybePlayer;
-    }
-
-    @Override
-    public Player save(Player player) {
-        Session session = sessionFactory.getCurrentSession();
-
-        Transaction transaction = session.beginTransaction();
-
-        session.persist(player);
-
-        session.getTransaction().commit();
-
-        return player;
+        return TransactionUtil.executeInTransaction(
+                session -> session.createQuery("select p from Player p  where p.name = :name", Player.class)
+                    .setParameter("name", playerName)
+                    .uniqueResultOptional(),
+                session
+        );
     }
 }
