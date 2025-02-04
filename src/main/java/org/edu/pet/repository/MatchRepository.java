45456293ed680facf6f7cd26a14806603a6585c5
Grigory Taran <grunder-96@mail.ru>
2025-02-04 09:10:48 +0300

    refactor: now MatchRepository is interface

--- src/main/java/org/edu/pet/repository/MatchRepository.java
+++ src/main/java/org/edu/pet/repository/MatchRepository.java
@@ -1,23 +1,5 @@
 package org.edu.pet.repository;
 
-import org.edu.pet.model.entity.Match;
-import org.edu.pet.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+public interface MatchRepository {
 
-public class MatchRepository {
-
-    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-
-    public Match save(Match match) {
-        Session session = sessionFactory.getCurrentSession();
-
-        session.beginTransaction();
-
-        session.persist(match);
-
-        session.getTransaction().commit();
-
-        return match;
-    }
 }
