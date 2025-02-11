18ca0316893d9bc62555c0e33465255fb39db3f1
Grigory Taran <grunder-96@mail.ru>
2025-02-11 17:22:59 +0300

    feature: add new methods in MatchMapper

--- src/main/java/org/edu/pet/mapper/MatchMapper.java
+++ src/main/java/org/edu/pet/mapper/MatchMapper.java
@@ -1,11 +1,14 @@
 package org.edu.pet.mapper;
 
+import org.edu.pet.dto.MatchResponseDto;
 import org.edu.pet.model.entity.Match;
 import org.edu.pet.model.match.MatchScore;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(uses = PlayerMapper.class)
 public interface MatchMapper {
@@ -18,4 +21,13 @@ public interface MatchMapper {
         @Mapping(source = "secondPlayer", target = "player2")
     })
     Match fromMatchScore(MatchScore matchScore);
+
+    @Mappings({
+        @Mapping(source = "player1.name", target = "firstPlayerName"),
+        @Mapping(source = "player2.name", target = "secondPlayerName"),
+        @Mapping(source = "winner.name", target = "winner")
+    })
+    MatchResponseDto fromMatch(Match match);
+
+    List<MatchResponseDto> fromMatches(List<Match> matches);
 }
