3b90bd483a1e7ab8f2733cab32fbe196433c2691
Grigory Taran <grunder-96@mail.ru>
2025-02-11 17:19:29 +0300

    refactor: removed the interface. Named the implementation after it.

--- src/main/java/org/edu/pet/repository/PlayerRepository.java
+++ src/main/java/org/edu/pet/repository/PlayerRepository.java
@@ -1,10 +1,27 @@
 package org.edu.pet.repository;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.edu.pet.model.entity.Player;
+import org.edu.pet.util.TransactionUtil;
 
 import java.util.Optional;
 
-public interface PlayerRepository {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PlayerRepository extends SharedRepositoryImpl<Player> {
 
-    Optional<Player> findByPlayerName(String playerName);
+    private static final PlayerRepository INSTANCE = new PlayerRepository();
+
+    public Optional<Player> findByPlayerName(String playerName) {
+        return TransactionUtil.executeInTransaction(
+                session -> session.createQuery("select p from Player p  where p.name = :name", Player.class)
+                    .setParameter("name", playerName)
+                    .uniqueResultOptional(),
+                session
+        );
+    }
+
+    public static PlayerRepository getInstance() {
+        return INSTANCE;
+    }
 }
