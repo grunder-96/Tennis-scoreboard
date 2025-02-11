5efdbdf5fd04a32f88ffc457cb3384548f923b7c
Grigory Taran <grunder-96@mail.ru>
2025-02-11 17:32:11 +0300

    fix: the service now uses an existing repository

--- src/main/java/org/edu/pet/service/PlayerService.java
+++ src/main/java/org/edu/pet/service/PlayerService.java
@@ -3,18 +3,18 @@ package org.edu.pet.service;
 import org.edu.pet.dto.PlayerDto;
 import org.edu.pet.model.entity.Player;
 import org.edu.pet.mapper.PlayerMapper;
-import org.edu.pet.repository.PlayerRepositoryImpl;
+import org.edu.pet.repository.PlayerRepository;
 
 import java.util.Optional;
 
 public class PlayerService {
 
-    private final PlayerRepositoryImpl playerRepositoryImpl = PlayerRepositoryImpl.getInstance();
+    private final PlayerRepository playerRepository = PlayerRepository.getInstance();
 
     public Player findOrSave(PlayerDto playerDto) {
-        Optional<Player> maybePlayer = playerRepositoryImpl.findByPlayerName(playerDto.getName());
+        Optional<Player> maybePlayer = playerRepository.findByPlayerName(playerDto.getName());
 
-        return maybePlayer.orElseGet(() -> playerRepositoryImpl.save(
+        return maybePlayer.orElseGet(() -> playerRepository.save(
                 PlayerMapper.INSTANCE.fromPlayerDto(playerDto))
         );
     }
