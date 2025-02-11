6dca71700e082f83afbf28d6b529f88ef82a9a41
Grigory Taran <grunder-96@mail.ru>
2025-02-11 17:00:56 +0300

    feature: add PaginationUtil

---
+++ src/main/java/org/edu/pet/util/PaginationUtil.java
@@ -0,0 +1,10 @@
+package org.edu.pet.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PaginationUtil {
+
+    public final int DEFAULT_PAGE_NUMBER = 1;
+    public final int DEFAULT_PAGE_SIZE = 5;
+}
